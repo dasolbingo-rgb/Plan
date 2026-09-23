9a3aa521a74a2a1f71089321d0d7f6b5aee14bd5
dasolbingo-rgb <dasolbingo@naver.com>
2026-09-23 22:37:59 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dasol-academy-v1';
+const CACHE='study-planner-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
